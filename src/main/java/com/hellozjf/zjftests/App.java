dbbe758a6b10945205731c2a0bec0a236137184f
hellozjf <908686171@qq.com>
2017-10-17 08:45:15 +0800

    Init zjftests.

---
+++ src/main/java/com/hellozjf/zjftests/App.java
@@ -0,0 +1,13 @@
+package com.hellozjf.zjftests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
